c0da1615cef52666257a6e44a27ae402e83acb7a
Vinicius Viegas <vuviegas85@gmail.com>
2020-09-22 17:29:27 -0300

    Routes edited for car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :user
-  has_many :trades, dependent: :destroy
-  has_many :posts,  dependent: :destroy
+  has_many :trades, dependent: :nullify
+  has_many :posts,  dependent: :nullify
 
   validates :brand, :model, :color, :description, :year, :price, presence: true
   validates :year, :price, numericality: true

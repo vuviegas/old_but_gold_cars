d7ceaa573458bf644a01e4a61f8b7381ffc9e422
Vinicius Viegas <vuviegas85@gmail.com>
2020-09-23 15:59:30 -0300

    Implemented the new and create for cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,4 +6,24 @@ class CarsController < ApplicationController
   def show
     @car = Car.find(params[:id])
   end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+    if @car.save
+      redirect_to cars_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :color, :year, :price, :description)
+  end
 end

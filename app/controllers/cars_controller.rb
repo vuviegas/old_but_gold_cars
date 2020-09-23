57b6803194f39487883dd21f2266290f9d992dfe
Mayssaa Khodr <mayus.tayus@gmail.com>
2020-09-23 15:57:04 -0300

    Show cars created

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,4 +1,6 @@
 class CarsController < ApplicationController
+  # before_action = :set_car, only: [:show]
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @cars = Car.all
   end
@@ -6,4 +8,10 @@ class CarsController < ApplicationController
   def show
     @car = Car.find(params[:id])
   end
+
+  private
+
+  # def set_car
+
+  # end
 end

8f31459f688c762886d8989c60891ee30a2bbbf9
Vinicius Viegas <vuviegas85@gmail.com>
2020-09-24 13:18:18 -0300

    implemented-cloudinary

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem 'devise'
 # Gem for .env file, to save API keys
 gem 'dotenv-rails', groups: [:development, :test]
 
+#Cloudinary gemfile, for images database
+gem 'cloudinary', '~> 1.16.0'
+
 group :development, :test do
   gem 'pry-byebug'
   gem 'pry-rails'
--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -40,7 +40,7 @@ class CarsController < ApplicationController
   private
 
   def car_params
-    params.require(:car).permit(:brand, :model, :color, :year, :price, :description)
+    params.require(:car).permit(:brand, :model, :color, :year, :price, :description, photos: [])
   end
 
   def set_car

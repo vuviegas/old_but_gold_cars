1d5fafa43d7a401427f4abecd793fe3665af721d
Vinicius Viegas <vuviegas85@gmail.com>
2020-09-25 14:03:18 -0300

    Implementing method for car buying

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,5 +1,23 @@
 class TradesController < ApplicationController
+
+
   def new
     @trade = Trade.new
+  end
+
+  def create
+    @trade = Trade.new(trade_params)
+    @trade.car = @car
+    if @trade.save
+      redirect_to new_car_trade_path(@car)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:trade).permit(:status, :car_id)
   end
 end
